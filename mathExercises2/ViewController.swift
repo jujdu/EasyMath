cf5422695625a67c25ff1c7c9442754a992c29b5
NewUser <jujdu@Jujdus-MacBook-Pro.local>
2018-10-03 23:10:41 +0300

    set new names for everythings

--- mathExercises2/ViewController.swift
+++ mathExercises2/ViewController.swift
@@ -9,25 +9,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet weak var goal: UILabel!
-    @IBOutlet weak var yourDecision: UIButton!
+    @IBOutlet weak var taskLabel: UILabel!
+    @IBOutlet weak var decisionLabel: UIButton!
     @IBOutlet weak var timerLabel: UILabel!
-    @IBOutlet weak var startResultButton: UIButton!
-    @IBOutlet weak var levelControlerSegment: UISegmentedControl!
+    @IBOutlet weak var startCheckButton: UIButton!
     @IBOutlet weak var scoreLabel: UILabel!
-    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    @IBOutlet weak var levelSegmentedControl: UISegmentedControl!
     
-    var timerSeconds = 0
-    var defaultTimerSeconds = SwitchLevel.easy.rawValue
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent // white color of status bar
+    }
+    
+    var secondsOfTimer = 0
+    var statedSecondsOfTimer = SwitchLevel.easy.rawValue
     var timer = Timer()
     var isTimeRunning = false
     var valueForRandomzer = 20
     var score = 0
     var strick = 0
     
-    var first = 0
-    var second = 0
-    var sum = 0
+    var firstNumber = 0
+    var secondNumber = 0
+    var sumOfNumbers = 0
     
     enum SwitchLevel: Int {
         case easy = 10
@@ -35,91 +38,92 @@ class ViewController: UIViewController {
         case hard = 6
     }
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        timerLabel.text = "\(statedSecondsOfTimer)"
+        resetStatusStartCheckButton()
+        decisionLabel.setTitle("", for: .normal)
+        decisionLabel.layer.cornerRadius = 10
+        decisionLabel.layer.masksToBounds = true
+        taskLabel.isHidden = true
+        enableZeroToNineButtons(false)
+        view.backgroundColor = #colorLiteral(red: 0.1333333333, green: 0.1568627451, blue: 0.1921568627, alpha: 1)
+        let font = UIFont.systemFont(ofSize: 20)
+        levelSegmentedControl.setTitleTextAttributes([NSAttributedStringKey.font: font],
+                                                     for: .normal)
+    }
+    
     func runTimer() {
         timer = Timer.scheduledTimer(timeInterval: 1, target: self,   selector: (#selector(ViewController.updateTimer)), userInfo: nil, repeats: true)
     }
     
     @objc func updateTimer() {
-        timerSeconds -= 1     //This will decrement(count down)the seconds.
-        timerLabel.text = "\(timerSeconds)" //This will update the label.
-        if timerSeconds == 0 {
+        secondsOfTimer -= 1     //decrement (count down) the seconds
+        timerLabel.text = "\(secondsOfTimer)" //update the label
+        if secondsOfTimer == 0 {
             showAlert(message: "You are late")
         }
     }
     
-    func mathGoal(value: Int) {
-        first = Int(arc4random_uniform(UInt32(value)) + 1)
-        second = Int(arc4random_uniform(UInt32(value)) + 1)
-        self.sum = first + second
+    func mathTask(value: Int) {
+        firstNumber = Int(arc4random_uniform(UInt32(value)) + 1)
+        secondNumber = Int(arc4random_uniform(UInt32(value)) + 1)
+        self.sumOfNumbers = firstNumber + secondNumber
     }
     func newRound() {
-        yourDecision.isHidden = false
-        goal.isHidden = false
-        mathGoal(value: valueForRandomzer)
-        startResultButton.setTitle("Check", for: .normal)
-        startResultButton.backgroundColor = #colorLiteral(red: 0, green: 0.6784313725, blue: 0.7098039216, alpha: 1)
-        startResultButton.setTitleColor(#colorLiteral(red: 0.9332413673, green: 0.9333977103, blue: 0.9332208037, alpha: 1), for: .normal)
-        goal.text = "\(first)+\(second)"
-        yourDecision.setTitle("", for: .normal)
-        timerSeconds = defaultTimerSeconds
-        timerLabel.text = "\(defaultTimerSeconds)"
+        decisionLabel.isHidden = false
+        taskLabel.isHidden = false
+        mathTask(value: valueForRandomzer)
+        startCheckButton.setTitle("Check", for: .normal)
+        startCheckButton.backgroundColor = #colorLiteral(red: 0, green: 0.6784313725, blue: 0.7098039216, alpha: 1)
+        startCheckButton.setTitleColor(#colorLiteral(red: 0.9332413673, green: 0.9333977103, blue: 0.9332208037, alpha: 1), for: .normal)
+        taskLabel.text = "\(firstNumber)+\(secondNumber)"
+        decisionLabel.setTitle("", for: .normal)
+        secondsOfTimer = statedSecondsOfTimer
+        timerLabel.text = "\(statedSecondsOfTimer)"
         runTimer()
-        changeButtonsActivity(true)
+        enableZeroToNineButtons(true)
     }
-    func changeButtonsActivity(_ isEnable: Bool) {
+    
+    func enableZeroToNineButtons(_ isEnable: Bool) {
         for buttonTag in 1...10 {
             let button = self.view.viewWithTag(buttonTag) as! UIButton
-            if isEnable == false {
-                button.layer.backgroundColor = #colorLiteral(red: 0.2235294118, green: 0.2431372549, blue: 0.2745098039, alpha: 0.3592947346)
-                button.titleLabel?.textColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.3564854452)
-            } else {
-                button.layer.backgroundColor = #colorLiteral(red: 0.282281518, green: 0.3065041304, blue: 0.3480320573, alpha: 1)
-                button.titleLabel?.textColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.7426423373)
-            }
             button.isEnabled = isEnable
+            switch button.isEnabled {
+            case true:
+                button.setTitleColor(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.7426423373), for: .disabled)
+                button.layer.backgroundColor = #colorLiteral(red: 0.282281518, green: 0.3065041304, blue: 0.3480320573, alpha: 1)
+            case false:
+                button.layer.backgroundColor = #colorLiteral(red: 0.2235294118, green: 0.2431372549, blue: 0.2745098039, alpha: 0.3592947346)
+                button.setTitleColor(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.3564854452), for: .disabled)
+            }
             button.layer.cornerRadius = 42
             button.layer.masksToBounds = true
-            
         }
     }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        timerLabel.text = "\(defaultTimerSeconds)"
-        setStatusStartResetButton()
-        yourDecision.setTitle("", for: .normal)
-        yourDecision.layer.cornerRadius = 10
-        yourDecision.layer.masksToBounds = true
-        goal.isHidden = true
-        changeButtonsActivity(false)
-        view.backgroundColor = #colorLiteral(red: 0.1333333333, green: 0.1568627451, blue: 0.1921568627, alpha: 1)
-        let font = UIFont.systemFont(ofSize: 20)
-        segmentedControl.setTitleTextAttributes([NSAttributedStringKey.font: font],
-                                                for: .normal)
+    func resetStatusStartCheckButton() {
+        startCheckButton.setTitle("Start", for: .normal)
+        startCheckButton.backgroundColor = #colorLiteral(red: 0.9332413673, green: 0.9333977103, blue: 0.9332208037, alpha: 1)
+        startCheckButton.setTitleColor(#colorLiteral(red: 0, green: 0.6784313725, blue: 0.7098039216, alpha: 1), for: .normal)
     }
     
-    func setStatusStartResetButton() {
-        startResultButton.setTitle("Start", for: .normal)
-        startResultButton.backgroundColor = #colorLiteral(red: 0.9332413673, green: 0.9333977103, blue: 0.9332208037, alpha: 1)
-        startResultButton.setTitleColor(#colorLiteral(red: 0, green: 0.6784313725, blue: 0.7098039216, alpha: 1), for: .normal)
-    }
-    
-    @IBAction func setDecision(_ sender: UIButton) {
-        yourDecision.imageView?.stopAnimating()
-        if yourDecision.currentTitle!.count != 4 {
-            yourDecision.setTitle(yourDecision.currentTitle! + sender.titleLabel!.text!, for: .normal)
-            yourDecision.isHidden = false
-            changeButtonsActivity(true)
-            if yourDecision.currentTitle!.count == 4 {
-                changeButtonsActivity(false)
+    @IBAction func setDecisionAction(_ sender: UIButton) {
+        decisionLabel.imageView?.stopAnimating()
+        if decisionLabel.currentTitle!.count != 4 {
+            decisionLabel.setTitle(decisionLabel.currentTitle! + sender.titleLabel!.text!, for: .normal)
+            decisionLabel.isHidden = false
+            enableZeroToNineButtons(true)
+            if decisionLabel.currentTitle!.count == 4 {
+                enableZeroToNineButtons(false)
             }
         }
     }
     
-    @IBAction func cleanValue(_ sender: UIButton) {
-        if yourDecision.currentTitle != "" {
-        yourDecision.setTitle(String(yourDecision.titleLabel!.text!.dropLast()), for: .normal)
-        changeButtonsActivity(true)
+    @IBAction func cleanValueAction(_ sender: UIButton) {
+        if decisionLabel.currentTitle != "" {
+        decisionLabel.setTitle(String(decisionLabel.titleLabel!.text!.dropLast()), for: .normal)
+        enableZeroToNineButtons(true)
         }
     }
     func showAlert(message: String) {
@@ -132,32 +136,26 @@ class ViewController: UIViewController {
         self.newRound()
         })
         
-        timer.invalidate()
+        timer.invalidate() // stop timer
         
         alert.addAction(action)
     
         present(alert, animated: true, completion: nil)
     }
     
-    @IBAction func changeComplexity(_ sender: UISegmentedControl) {
-        setStatusStartResetButton()
-        timer.invalidate()
-        timerLabel.text = "\(defaultTimerSeconds)"
-    }
-    
-    @IBAction func checkResult(_ sender: UIButton) {
-        if startResultButton.titleLabel?.text == "Start" {
+    @IBAction func checkResultOfTaskAction(_ sender: UIButton) {
+        if startCheckButton.titleLabel?.text == "Start" {
             newRound()
         } else {
-        Int(yourDecision.currentTitle!) == sum ?
+        Int(decisionLabel.currentTitle!) == sumOfNumbers ?
             showAlert(message: "You are right!") :
             showAlert(message: "You are not right, try again!")
         }
     }
     func addScore () {
-        if Int(yourDecision.currentTitle!) == sum && timerSeconds != 0 {
+        if Int(decisionLabel.currentTitle!) == sumOfNumbers && secondsOfTimer != 0 {
             self.strick += 1
-            self.score += (2 + timerSeconds * strick) / 3
+            self.score += (2 + secondsOfTimer * strick) / 3
             scoreLabel.text = String(score)
         } else {
             self.strick = 0
@@ -167,32 +165,32 @@ class ViewController: UIViewController {
     }
     
     @IBAction func switchLevelAction(_ sender: UISegmentedControl) {
-        changeButtonsActivity(false)
-        goal.isHidden = true
+        resetStatusStartCheckButton()
+        timer.invalidate() // stop timer
+        timerLabel.text = "\(statedSecondsOfTimer)"
+        enableZeroToNineButtons(false)
+        taskLabel.isHidden = true
+        
         self.score = 0
         scoreLabel.text = String(score)
-        yourDecision.setTitle("", for: .normal)
+        decisionLabel.setTitle("", for: .normal)
+        
         switch sender.selectedSegmentIndex {
         case 0:
-            self.defaultTimerSeconds = SwitchLevel.easy.rawValue
+            self.statedSecondsOfTimer = SwitchLevel.easy.rawValue
             self.valueForRandomzer = 20
-            timerLabel.text = "\(defaultTimerSeconds)"
+            timerLabel.text = "\(statedSecondsOfTimer)"
         case 1:
-            self.defaultTimerSeconds = SwitchLevel.normal.rawValue
+            self.statedSecondsOfTimer = SwitchLevel.normal.rawValue
             self.valueForRandomzer = 100
-            timerLabel.text = "\(defaultTimerSeconds)"
+            timerLabel.text = "\(statedSecondsOfTimer)"
         case 2:
-            self.defaultTimerSeconds = SwitchLevel.hard.rawValue
+            self.statedSecondsOfTimer = SwitchLevel.hard.rawValue
             self.valueForRandomzer = 500
-            timerLabel.text = "\(defaultTimerSeconds)"
+            timerLabel.text = "\(statedSecondsOfTimer)"
         default:
             break
         }
-    }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 }
 
